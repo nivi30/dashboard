e0b44f076fb37711e916cee7607190b35f98b7ee
Nivetha V <98044718+nivi30@users.noreply.github.com>
2023-09-25 18:48:36 +0530

    Create find_max_min.py

---
+++ find_max_min.py
@@ -0,0 +1,23 @@
+import requests
+
+repository_owner = "nivi30"
+repository_name = "dashboard"
+access_token = "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"
+
+url=f"https://api.github.com/repos/{repository_owner}/{repository_name}/actions/runs"
+headers = { "Authorization": f"token {access_token}" }
+response = requests.get(url, headers=headers)
+workflow_runs = response.json()["workflow_runs"]
+
+max_run_time = None
+min_run_time = None
+
+for run in workflow_runs:
+  duration = run["run_duration"]
+  if max_run_time is None or duration > max_run_time:
+    max_run_time = duration
+  if min_run_time is None or duration < min_run_time:
+    min_run_time = duration
+
+print(f"Maximum run time : {max_run_time} seconds")
+print(f"Minimum run time : {min_run_time} seconds")
